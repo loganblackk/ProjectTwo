b80a28c363c55c0f1cc24337066f1a3962b35d53
Michael Logan Foy <loganblack0@gmail.com>
2020-03-16 14:18:27 -0700

    updating again

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const app = express();
 require('dotenv').config();
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
-mongoose.Promise = global.Promise
 const db = mongoose.connection;
-const beachesController = require('./controllers/beachesController.js')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+
+
+const beachesController = require('./controllers/beachesController.js')
 app.use('/beaches', beachesController)
+
 
 const dbupdateobject = {
   useNewUrlParser: true,
@@ -25,68 +27,9 @@ db.on('open', () => {
     console.log('Connection made!');
 });
 
-// app.get('/beaches', (req, res) => {
-//     res.send('your application is working');
-// });
-
-app.put('/:id', (req, res) => {
-  Beaches.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedBeaches) => {
-    res.redirect(`/beaches/${req.params.id}`)
-  });
-  })
-  
-// *edit route
-app.get('/beaches/:id/edit', (req, res) => {
-  Beaches.findById(req.params.id, (err, items) => {
-    res.render('edit.ejs', { beaches: items });
-  });
+app.get('/', (req, res) => {
+    res.send('your application is working');
 });
-//*remove route
-app.delete('/beaches/:id', (req, res) => {
-  Beaches.findByIdAndRemove(req.params.id, (err, data) => {
-    res.redirect('/beaches');
-  });
-});
-//*New route
-app.get('/beaches/new', (req, res) => {
-  res.render('new.ejs')
-})
-
-//*show route
-app.get('/beaches/:id', (req, res) => {
-  Beaches.findById(req.params.id, (error, item) => {
-    res.render('show.ejs', { beaches: item })
-  })
-})
-
-//*Index route
-app.get('/beaches', (req, res) => {
-  Beaches.find({}, (error, items) => {
-    res.render('index.ejs', { beaches: items })
-  })
-}) 
-
-
-
-//* created route
-app.post('/beaches/', (req, res) => {
-Beaches.create(req.body, (error, createdBeach) => {
-    res.redirect('/beaches');
-  })
-})
-
-
-
-//*edit qty route
-app.put('/:id/qty', (req, res) => {
-  Beaches.findByIdAndUpdate(req.params.id,{$inc: {qty:+1}}, 
-    (err, updatedBeachess) => {
-      res.redirect(`/beaches/${req.params.id}`)
-    });
-});
-
-
-
 
 
 //listen for requests
