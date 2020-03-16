fe33ac0caa1172b31ce4797992a13ab936ec39f9
Michael Logan Foy <loganblack0@gmail.com>
2020-03-15 18:19:42 -0700

    trying again

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-
+const PORT= process.env.PORT || 5000;
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const db = mongoose.connection;
